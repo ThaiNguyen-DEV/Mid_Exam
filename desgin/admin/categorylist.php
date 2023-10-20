45b09feef373e695d1748822b563fcec29f021c7
Duy Thái <nguyendinhduythai42@gmail.com>
2023-10-20 07:44:56 +0700

    first

---
+++ desgin/admin/categorylist.php
@@ -0,0 +1,44 @@
+<?php
+include("header.php");
+include("slider.php");
+include("class/category_class.php")
+?>
+
+<?php
+$category = new category;
+$show_category = $category->show_category();
+?>
+
+
+<div class="admin-content-right">
+    <div class="admin-content-right-category_list">
+        <h1>Danh sách danh mục</h1>
+        <table>
+            <tr>
+                <th>STT</th>
+                <th>ID</th>
+                <th>Danh mục</th>
+                <th>Tuỳ biến</th>
+            </tr>
+            <?php
+            if($show_category){ $i=0;
+                while( $result = $show_category->fetch_assoc()) {$i++;
+
+            ?>
+            <tr>
+                <td><?php echo $i?></td>
+                <td><?php echo $result['category_id'] ?></td>
+                <td><?php echo $result['category_name'] ?></td>
+                <td><a href="#">Sửa</a> <a href="#">Xoá</a></td>
+            </tr>
+            <?php
+                }
+            }
+            ?>
+        </table>
+    </div>
+</div>
+</section>
+</body>
+
+</html>
