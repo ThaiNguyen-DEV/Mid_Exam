515c681f20dc338536567ffe8a6d525919b955e5
Duy Thái <nguyendinhduythai42@gmail.com>
2023-10-22 17:42:12 +0700

    done

--- desgin/orders.php
+++ desgin/orders.php
@@ -46,11 +46,11 @@ if(isset($_SESSION['user_id'])){
    <div class="box-container">
 
       <div class="box">
-         <p> placed on : <span>5-29-2022</span> </p>
-         <p> name : <span>shaikh anas</span> </p>
-         <p> number : <span>1234567899</span> </p>
-         <p> email : <span>shaikhanas@gmail.com</span> </p>
-         <p> address : <span>jogeshwari, mumbai, india - 400103</span> </p>
+         <p> placed on : <span>10-21-2023</span> </p>
+         <p> name : <span>Duy Thai</span> </p>
+         <p> number : <span>123456789</span> </p>
+         <p> email : <span>thaindd.22git@vku.udn.vn</span> </p>
+         <p> address : <span>Da Nang</span> </p>
          <p> your orders : <span>pizza 01 (1) - main dish 02 (3) -</span> </p>
          <p> total price : <span>$12/-</span> </p>
          <p> payment method : <span>cash on delivery</span> </p>
@@ -58,11 +58,11 @@ if(isset($_SESSION['user_id'])){
       </div>
 
       <div class="box">
-         <p> placed on : <span>5-29-2022</span> </p>
-         <p> name : <span>shaikh anas</span> </p>
-         <p> number : <span>1234567899</span> </p>
-         <p> email : <span>shaikhanas@gmail.com</span> </p>
-         <p> address : <span>jogeshwari, mumbai, india - 400103</span> </p>
+         <p> placed on : <span>10-21-2023</span> </p>
+         <p> name : <span>Nhat Lam</span> </p>
+         <p> number : <span>123456789</span> </p>
+         <p> email : <span>lamtn.22git@vku.udn.vn</span> </p>
+         <p> address : <span>Da Nang</span> </p>
          <p> your orders : <span>pizza 01 (1) - main dish 02 (3) -</span> </p>
          <p> total price : <span>$12/-</span> </p>
          <p> payment method : <span>cash on delivery</span> </p>
